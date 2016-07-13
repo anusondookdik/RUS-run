f9d83e26d3484d8a273bada635a15d8c6720df3e
anuson <dookdikzahiso@gmail.com>
2016-07-13 10:23:17 +0700

    กรอกข้อมูลไม่ครบ

---
+++ app/src/main/java/com/duangsangrit/anuson/rusrun/MyAlert.java
@@ -0,0 +1,31 @@
+package com.duangsangrit.anuson.rusrun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by HippoDik on 13/7/2559.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+
+} // Main Class
--- app/src/main/java/com/duangsangrit/anuson/rusrun/SignUpActivity.java
+++ app/src/main/java/com/duangsangrit/anuson/rusrun/SignUpActivity.java
@@ -2,12 +2,62 @@ package com.duangsangrit.anuson.rusrun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.RenamingDelegatingContext;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+
+    //Explicit
+
+    private EditText nameEditText, userEditText, passwordEditext;
+    private RadioGroup RadioGroup;
+    private RadioButton  avata0RedioButton,avata1RedioButton ,avata2RedioButton,avata3RedioButton,
+            avata4RedioButton;
+    private String nameString, userString, passwordString, avataString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+
+        //Bind Widget
+        nameEditText = (EditText)findViewById(R.id.editText);
+        userEditText = (EditText)findViewById(R.id.editText2);
+        passwordEditext =(EditText)findViewById(R.id.editText3);
+        RadioGroup = (RadioGroup) findViewById(R.id.radAvsta);
+        avata0RedioButton = (RadioButton)findViewById(R.id.radioButton);
+        avata1RedioButton = (RadioButton)findViewById(R.id.radioButton2);
+        avata2RedioButton = (RadioButton)findViewById(R.id.radioButton3);
+        avata3RedioButton = (RadioButton)findViewById(R.id.radioButton4);
+        avata4RedioButton = (RadioButton)findViewById(R.id.radioButton5);
+
+
+
+    } //Main Method
+
+
+    public void  clickSignUpSign(View view ) {
+
+        // Get value from Edit Text
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditext.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง","กรุณากรอกทุกช่อง อิสัส");
+        }
+
+    } //clickSignUp
+
+}  // Main Class
