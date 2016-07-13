269ef87a7ad3e3f3805b29d228850794f92dab50
anuson <dookdikzahiso@gmail.com>
2016-07-13 11:12:18 +0700

    Check  Choose

--- app/src/main/java/com/duangsangrit/anuson/rusrun/SignUpActivity.java
+++ app/src/main/java/com/duangsangrit/anuson/rusrun/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.duangsangrit.anuson.rusrun;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.test.RenamingDelegatingContext;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
@@ -16,7 +17,7 @@ public class SignUpActivity extends AppCompatActivity {
     //Explicit
 
     private EditText nameEditText, userEditText, passwordEditext;
-    private RadioGroup RadioGroup;
+    private RadioGroup radioGroup;
     private RadioButton  avata0RedioButton,avata1RedioButton ,avata2RedioButton,avata3RedioButton,
             avata4RedioButton;
     private String nameString, userString, passwordString, avataString;
@@ -31,13 +32,46 @@ public class SignUpActivity extends AppCompatActivity {
         nameEditText = (EditText)findViewById(R.id.editText);
         userEditText = (EditText)findViewById(R.id.editText2);
         passwordEditext =(EditText)findViewById(R.id.editText3);
-        RadioGroup = (RadioGroup) findViewById(R.id.radAvsta);
+        radioGroup = (RadioGroup) findViewById(R.id.radAvsta);
         avata0RedioButton = (RadioButton)findViewById(R.id.radioButton);
         avata1RedioButton = (RadioButton)findViewById(R.id.radioButton2);
         avata2RedioButton = (RadioButton)findViewById(R.id.radioButton3);
         avata3RedioButton = (RadioButton)findViewById(R.id.radioButton4);
         avata4RedioButton = (RadioButton)findViewById(R.id.radioButton5);
 
+        //Radio Controller
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+
+                    case R.id.radioButton:
+
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+
+                        avataString ="3";
+                        break;
+                    case R.id.radioButton5:
+
+                        avataString ="4";
+                        break;
+
+                }   //switch
+
+            }
+        });
 
 
     } //Main Method
@@ -56,8 +90,36 @@ public class SignUpActivity extends AppCompatActivity {
 
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "มีช่องว่าง","กรุณากรอกทุกช่อง อิสัส");
+
+        } else if (checkChoose()) {
+            //Checked
+        } else {
+
+            //Un Check
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog (this, "ยังไม่เลือก Avata","กรุณาเลือก Avata อิสัส ");
+
+
         }
 
+
+
+
     } //clickSignUp
+
+    private boolean checkChoose() {
+
+        boolean status = true;
+
+        status = avata0RedioButton.isChecked() ||
+                avata1RedioButton.isChecked() ||
+                avata2RedioButton.isChecked() ||
+                avata3RedioButton.isChecked() ||
+                avata4RedioButton.isChecked();
+
+        Log.d("test","stauts == " + status);
+
+        return status;
+    }
 
 }  // Main Class
